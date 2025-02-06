e2961f448cf6a2ab32fc7606f1bafc97f2435335
Zeynep Bedia Ayan <zeynaya@dfds.com>
2025-02-06 12:02:04 +0300

    permission fix #4

--- tests/button-click.spec.js
+++ tests/button-click.spec.js
@@ -1,15 +1,20 @@
-const { test, expect, chromium } = require('@playwright/test');
+const {test, expect, chromium} = require('@playwright/test');
+const path = require('path');
 
 test('Button Click Test', async () => {
-  const browser = await chromium.launch({ headless: true }); // ✅ Set headless mode
+  const browser = await chromium.launch({headless: false});
   const context = await browser.newContext();
-  const page = await context.newPage();
+  const page =  await context.newPage();
 
-  await page.goto('https://amazon.com');
+  const filePath = path.join(__dirname, 'test-page.html');
+  const fuleUrl = `file://${filePath}`;
+
+  await page.goto(fileUrl);
+
   const button = await page.locator('button#myButton');
   await expect(button).toBeVisible();
   await button.click();
   await expect(button).toHaveText('Clicked');
 
   await browser.close();
-});
+})
